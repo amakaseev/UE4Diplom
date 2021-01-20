8fa21637fb2e9e761373a053da844ce26e3a1e43
hazed <hazed@syneforge.com>
2021-01-20 15:37:27 +0200

    add c++

---
+++ Source/RLS/RLS.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "RLS.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RLS, "RLS" );
